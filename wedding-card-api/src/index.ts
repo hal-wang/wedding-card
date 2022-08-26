bcf716d4090e3989a2276d7f5183f46019006903
hal-wang <hi@hal.wang>
2022-08-26 19:49:26 +0800

    chore: update index

--- wedding-card-api/src/index.ts
+++ wedding-card-api/src/index.ts
@@ -2,12 +2,4 @@ import { LambdaStartup } from "@ipare/lambda";
 import startup from "./startup";
 
 const app = startup(new LambdaStartup(), "production");
-export const main = async (
-  event: Record<string, unknown>,
-  context: Record<string, unknown>
-): Promise<unknown> => {
-  console.log("event", JSON.stringify(event));
-  console.log("context", JSON.stringify(context));
-
-  return await app.run(event, context);
-};
+export const main = (event, context) => app.run(event, context);
